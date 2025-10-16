4248abca5a5a7d580800033b9f3f32bc67b2de23
Hidayat3118 <muhammadhidayat3118@gmail.com>
2025-10-16 21:10:21 +0800

    database sudah

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaksi extends Model
 {
-    //
+    protected $guarded = ['id'];
+    public function produk()
+    {
+        return $this->belongsToMany(Produk::class, 'produk_transaksi')->withPivot("qty", 'subtotal')->withTimestamps();
+    }
 }
